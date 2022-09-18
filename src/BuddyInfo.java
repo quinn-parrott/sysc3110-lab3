d408e79211cd319e524a8db0b70fc692bbb80f09
Quinn Parrott <quinn.parrott@gmail.com>
2022-09-18 17:02:06 -0600

    Main will now add then remove a buddy from an address book

--- src/BuddyInfo.java
+++ src/BuddyInfo.java
@@ -36,6 +36,9 @@ public class BuddyInfo {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address Book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
